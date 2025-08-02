11dcd5f8b3032587d411e2e4e6c793ee76c32e58
Abhishek Dixit <153361834+thenameisshivaay@users.noreply.github.com>
2025-08-02 15:43:03 +0530

    Add files via upload

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,1 +1,26 @@
+import jwt from 'jsonwebtoken';
 
+const userAuth =async (req, res, next) => {
+    const { token } = req.headers;
+
+
+    if (!token) {
+        return res.json({ success: false, message: 'Not Authorized. Login Again' });
+    }
+    try {
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+        if(tokenDecode.id){
+            req.body = req.body || {};
+            req.body.userId = tokenDecode.id;
+
+            next();
+        } else{
+            return res.json({ success: false, message: 'Not Authorized. Login Again' });
+        }
+        
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+    }
+};
+
+export default userAuth;
