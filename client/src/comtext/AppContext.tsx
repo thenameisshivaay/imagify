dd456fd3a1c9dc6155a344ce47e26f25ac5881f1
Abhishek Dixit <153361834+thenameisshivaay@users.noreply.github.com>
2025-08-02 15:34:43 +0530

    Add files via upload

--- client/src/comtext/AppContext.tsx
+++ client/src/comtext/AppContext.tsx
@@ -1,1 +1,94 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
+
+export const AppContext = createContext()
+
+const AppContextProvider = (props) =>{
+    const [user ,setUser] = useState(null);
+    const [token, setToken] =useState (localStorage.getItem('token'))
+
+    const[credit,setCredit] = useState(false)
+
+    const [showLogin, setShowLogin] = useState(false)
+    const navigate =useNavigate()    
+    
+    const backendUrl=import.meta.env.VITE_BACKEND_URL
+
+    const loadCreditsData = async () => {
+        try {
+
+            const {data} = await axios.get(backendUrl+'/api/user/credits' , {headers: {token}})
+
+            if(data.success){
+                setCredit(data.credits)
+                setUser(data.user)
+            }
+            
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+            
+        }
+    }
+
+    const generateImage = async (prompt) =>
+    {
+        try {
+
+            const{data}=await axios.post(backendUrl+'/api/image/generate-image', {prompt} , {headers: {token}})
+
+            if(data.success){
+                loadCreditsData()
+                return data.resultImage
+            } else{
+                toast.error(data.message)
+                loadCreditsData()
+                if (data.creditBalance === 0){
+                    navigate('/buy-credit')
+                     return;
+
+                }
+            }
+            
+        } catch (error) {
+            toast.error(error.message)
+            
+        }
+
+    }
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        setToken('')
+        setUser(null)
+
+    }
+
+    useEffect(()=>{
+        if(token){
+            loadCreditsData()
+        }
+
+    },[token])
+ 
+    const value = {
+        user,
+        setUser ,
+        showLogin,
+        setShowLogin,
+        backendUrl,
+        token, setToken,
+        credit,setCredit,loadCreditsData,logout,generateImage
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppContextProvider;
